7b5ff705a5123a04db982fd0af7c480ed1c98ef3
maxiaobu1999 <maqinglong1999@163.com>
2019-07-17 10:29:48 +0800

    【jni】cmake环境配置

---
+++ app/src/main/java/com/norman/ffmpegguide/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.norman.ffmpegguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HomeActivity extends AppCompatActivity {
+    public static final String TAG = "HomeActivity+++:";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+//        System.loadLibrary("ijkffmpeg");
+        NativeUtil nativeUtil = new NativeUtil();
+        String s = nativeUtil.hello();
+        Log.d(TAG, s);
+    }
+
+
+}
